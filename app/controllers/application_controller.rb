d7ed802547e6ee0b30b6bfe77f65ee3b9630bf4f
scbq <felipe.aguilera.fuentealba@gmail.com>
2024-10-13 18:30:22 -0300

    configuracion de cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,20 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  # Require authentication for all actions by default
   before_action :authenticate_user!
 
+  # Handle unauthorized access
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: "No tienes permiso para realizar esta acción."
+  end
+
+  # Redirect user after signing in
   def after_sign_in_path_for(resource)
     root_path
   end
 
+  # Redirect user after signing out
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
